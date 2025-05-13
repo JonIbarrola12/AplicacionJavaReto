dabe3b879d2a5f1b6b14ca2f4e987d2588ad262a
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-05-13 11:13:12 +0200

    Arreglo de borrar ejemplares

--- reto/Io.java
+++ reto/Io.java
@@ -955,18 +955,22 @@ public class Io{
         String sql = "DELETE FROM ejemplares WHERE cod_ejem = ?";
         sop("Introduce el codigo del ejemplar a eliminar:");
         String codEjem = scanner.nextLine();
-        if (comprobarEjemplares(conn,scanner,codEjem)){
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, codEjem);
-                int rowsAffected = stmt.executeUpdate();
-                if (rowsAffected > 0) {
-                    sop("Ejemplar eliminado correctamente.");
-                } else {
-                    sop("No se encontro ningun ejemplar con ese codigo.");
+        if (comprobarEjemplarEnPrestamo(conn,scanner,codEjem)){
+            if (comprobarCantidadEjemplares(conn,scanner,codEjem)){
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    stmt.setString(1, codEjem);
+                    int rowsAffected = stmt.executeUpdate();
+                    if (rowsAffected > 0) {
+                        sop("Ejemplar eliminado correctamente.");
+                    } else {
+                        sop("No se encontro ningun ejemplar con ese codigo.");
+                    }
                 }
+            }else{
+                sop("No se puede eliminar el ejemplar ya que es el ultimo que queda de ese libro");
             }
         }else{
-            sop("No se puede eliminar el ejemplar ya que es el ultimo que queda de ese libro");
+            sop("No se pudo eliminar el ejemplar ya que esta en prestamo o reparacion");
         }
     }
     public static void eliminarAutor(Connection conn, Scanner scanner) throws SQLException {
@@ -984,7 +988,7 @@ public class Io{
             }
         }
     }
-    public static boolean comprobarEjemplares(Connection conn, Scanner scanner, String codEjem){
+    public static boolean comprobarCantidadEjemplares(Connection conn, Scanner scanner, String codEjem){
         String sql = "SELECT * FROM ejemplares WHERE cod_ejem = ?";
         int cont=0;
         try (PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -997,6 +1001,17 @@ public class Io{
             sop("Error al eliminar el ejemplar: "+ e.getMessage());
         }
         return cont<2;
+    }
+    public static boolean comprobarEjemplarEnPrestamo(Connection conn, Scanner scanner, String codEjem){
+        String sql ="SELECT * FROM ejemplares WHERE cod_ejem = ? AND estado != 'disponible'";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, codEjem);
+            ResultSet rs =stmt.executeQuery();
+            return rs.next();
+        }catch(SQLException e){
+            sop("Error al comprobar el estado de los ejemplares: "+ e.getMessage());
+            return false;
+        }
     }
     public static boolean comprobarCodigoUsuario(Connection conn, int codigo) throws SQLException {
         String sql = "SELECT * FROM usuarios WHERE cod_usuario = ?";
